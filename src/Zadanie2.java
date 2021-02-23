9a0917aff72cabce474541663729df4c0122fe50
BastianSchmitt <bastibasti37@gmail.com>
2021-02-23 17:47:50 +0100

    22.02.20

--- src/Zadanie2.java
+++ src/Zadanie2.java
@@ -1,3 +1,8 @@
 public class Zadanie2 {
-
+    public static void main (String[] args){
+        int myIntA = 10;
+        int myIntB = 5;
+        int myIntC = myIntA * myIntA + myIntB * myIntB;
+        System.out.println("Wynik =" + myIntC);
+    }
 }
