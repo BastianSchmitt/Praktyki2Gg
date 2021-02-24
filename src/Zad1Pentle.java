74671027075ed94494830abc00ce3e56db37e938
BastianSchmitt <bastibasti37@gmail.com>
2021-02-24 23:08:05 +0100

    23.02.20

---
+++ src/Zad1Pentle.java
@@ -0,0 +1,11 @@
+public class Zad1Pentle {
+    public static void main(String[] args){
+        char firstLetterOfAnAlphabet = 'A';
+        for (int i = 0; i < 26; i++){
+            if(i % 2 ==0)
+                System.out.print(firstLetterOfAnAlphabet + "\t");
+            firstLetterOfAnAlphabet ++;
+
+        }
+    }
+}
