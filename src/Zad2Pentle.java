5d28525eb58718545699a91ec5c01570e6466c17
BastianSchmitt <bastibasti37@gmail.com>
2021-02-25 09:55:13 +0100

    23.02.20

---
+++ src/Zad2Pentle.java
@@ -0,0 +1,21 @@
+public class Zad2Pentle {
+    public static void main(String[] args) {
+
+        int x = 9;
+        int pierwsza = 0;
+        int druga = 1;
+        int Fibonacci;
+
+
+
+        for(int i = 1; i <= x; i++){
+
+
+            Fibonacci = pierwsza + druga;
+            System.out.print(Fibonacci + "\t");
+            pierwsza = druga;
+            druga = Fibonacci;
+
+        }
+    }
+}
