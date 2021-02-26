ede1b7807625ab3e867786a4c30f08222b7e59d5
BastianSchmitt <bastibasti37@gmail.com>
2021-02-26 10:12:26 +0100

    25.02.20

---
+++ src/Zad3Pentle.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Zad3Pentle{
+    public static void main(String[] args) {
+        //Zadanie 3
+        Scanner sc = new Scanner(System.in);
+
+        boolean programdziala = true;
+
+        while (programdziala) {
+            System.out.println("Podaj numer drzwi: ");
+            String userInput = sc.next();
+
+            switch (userInput) {
+                case "1": {
+                    System.out.println("Złe drzwi, wybierz inne");
+                    break;
+                }
+                case "2": {
+                    System.out.println("Złe drzwi, wybierz inne");
+                    break;
+                }
+                case "3": {
+                    System.out.println("Super wybrałeś popeawne drzwi");
+                    programdziala = false;
+                    break;
+                }
+                case "4": {
+                    System.out.println("Złe drzwi, wybierz inne");
+                    break;
+                }
+                case "5": {
+                    System.out.println("Złe drzwi, wybierz inne");
+                    break;
+                }
+                default: {
+                    System.out.println("Podałeś złą liczbę");
+                }
+            }
+        }
+    }
+}
---
+++ src/Zad5Pentle.java
@@ -0,0 +1,14 @@
+public class Zad5Pentle {
+    public static void main(String[] args) {
+        int wysokoscTrojkata = 8;
+
+
+        for (int z = 0; z < wysokoscTrojkata; z++) {
+            for (int v = 0; v <= z; v++) {
+                System.out.print("X ");
+            }
+            System.out.println();
+        }
+    }
+}
+
